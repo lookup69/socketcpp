ef7fb60ede5030ab29e53dceaca58133043bbfb4
Archer Chang <archerchang@qnap.com>
2023-10-18 15:31:15 +0800

    update

--- IfaceHelper.hpp
+++ IfaceHelper.hpp
@@ -13,35 +13,43 @@ namespace lkup69
 
 class IfaceInfo final
 {
+        const static size_t kIFaceInfoField_NAME = 0;
+        const static size_t kIFaceInfoField_IP   = 1;
+        const static size_t kIFaceInfoField_MASK = 2;
+        const static size_t kIFaceInfoField_MAC  = 3;
+
 public:
         IfaceInfo(const std::string &name,
                   const std::string &ip,
                   const std::string &mask,
                   const std::string &mac)
-                : m_iface(name, ip, mask, mac)
         {
+                std::get<kIFaceInfoField_NAME>(m_iface) = name;
+                std::get<kIFaceInfoField_IP>(m_iface)   = ip;
+                std::get<kIFaceInfoField_MASK>(m_iface) = mask;
+                std::get<kIFaceInfoField_MAC>(m_iface)  = mac;
         }
 
         ~IfaceInfo() = default;
 
         std::string GetName(void)
         {
-                return std::get<0>(m_iface);
+                return std::get<kIFaceInfoField_NAME>(m_iface);
         }
 
         std::string GetIp(void)
         {
-                return std::get<1>(m_iface);
+                return std::get<kIFaceInfoField_IP>(m_iface);
         }
 
         std::string GetMask(void)
         {
-                return std::get<2>(m_iface);
+                return std::get<kIFaceInfoField_MASK>(m_iface);
         }
 
         std::string GetMac(void)
         {
-                return std::get<3>(m_iface);
+                return std::get<kIFaceInfoField_MAC>(m_iface);
         }
 
 private:
